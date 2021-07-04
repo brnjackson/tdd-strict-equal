bbb188ae774765100673a1b2181954a771b60559
Brittney Jackson <brnjackson@yahoo.com>
2021-07-04 17:40:39 -0400

    fixed formatting on all files

--- tests/calculation.test.js
+++ tests/calculation.test.js
@@ -1,23 +1,23 @@
 let describe = require('mocha').describe
-let it = require('mocha').itgit
+let it = require('mocha').it
 let expect = require ('chai').expect
 let strictEqual = require('../calculation.js')
 
 describe ('equality',() => {
-    describe ('strictEqualsTest', () => {
-        it ('numbers are strictly equal', () => {
-        let strictEqualResult = strictEqual (1,1)
-        expect (strictEqualResult).to.equal(true)
-        })
-
-        it('strings are strictly equal',() => {
-          let strictEqualResult = strictEqual ('six','six')
-          expect(strictEqualResult).to.equal(true)
-        })
-
-        it('strings are not strictly equal',() => {
-          let strictEqualResult = strictEqual('six','six')
-          expect(strictEqualResult).to.equal(false)
-        })
+  describe ('strictEqualsTest', () => {
+    it ('numbers are strictly equal', () => {
+      let strictEqualResult = strictEqual (5,5)
+      expect (strictEqualResult).to.equal(true)
     })
+
+    it('strings are strictly equal',() => {
+      let strictEqualResult = strictEqual ('six','six')
+      expect(strictEqualResult).to.equal(true)
+    })
+
+    it('strings are not strictly equal',() => {
+      let strictEqualResult = strictEqual('six','ten')
+      expect(strictEqualResult).to.equal(false)
+    })
+  })
 })
